bdc91ea501ebba179a40d50a3db5b03f68290501
DTGuy <tguy5837@gmail.com>
2022-12-13 22:15:01 -0500

    connected front end to back end

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 // import Apollo server
 const { ApolloServer } = require('apollo-server-express');
@@ -7,7 +8,7 @@ const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas')
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
@@ -18,6 +19,15 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(xpress.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {

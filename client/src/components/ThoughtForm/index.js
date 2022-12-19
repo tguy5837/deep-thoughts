ec479684888a25ff2bf709d4df8bb74580717a78
DTGuy <tguy5837@gmail.com>
2022-12-18 19:55:54 -0500

    added thought and reaction forms for creating

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,8 +1,38 @@
 import React, { useState } from "react";
 
+import { useMutation } from "@apollo/client";
+import { ADD_THOUGHT } from "../../utils/mutations";
+import { QUERY_THOUGHTS, QUERY_ME } from "../../utils/queries";
+
 const ThoughtForm = () => {
     const [thoughtText, setThoughtText] = useState('');
     const [characterCount, setCharacterCount] = useState(0);
+
+    // why is the first part in an array?
+    // what is this syntax for updating?
+    const [addThought, { error }] = useMutation(ADD_THOUGHT, {
+        update(cache, { data: addThought }) {
+
+            // could potentially not exist yet, so wrap in a try/catch
+            try {
+                // update me array's cache
+                const { me } = cache.readQuery({ query: QUERY_ME });
+                cache.writeQuery({
+                    query: QUERY_ME,
+                    data: { me: { ...me, thoughts: [...me.thoughts, addThought] } },
+                });
+            } catch (e) {
+                console.warn("First thought insertion by user!")
+            }
+
+            // update thought array's cache
+            const { thoughts } = cache.readQuery({ query: QUERY_THOUGHTS });
+            cache.writeQuery({
+                query: QUERY_THOUGHTS,
+                data: { thoughts: [addThought, ...thoughts] },
+            });
+        }
+    });
 
     const handleChange = event => {
         if (event.target.value.length <= 280) {
@@ -10,18 +40,30 @@ const ThoughtForm = () => {
             setCharacterCount(event.target.value.length);
 
         }
-    }
+    };
 
     const handleFormSubmit = async event => {
         event.preventDefault();
-        setThoughtText('');
-        setCharacterCount(0);
-    }
+
+        try {
+            // add thought to database
+            await addThought({
+                variables: { thoughtText }
+            });
+
+            // clear form values
+            setThoughtText('');
+            setCharacterCount(0);
+        } catch (e) {
+            console.error(e);
+        }
+    };
 
     return (
         <div>
-            <p className={`m-0 ${characterCount === 280 ? 'text-error' : ''}`}>
+            <p className={`m-0 ${characterCount === 280 || error ? 'text-error' : ''}`}>
                 Character Count: {characterCount}/280
+                {error && <span className="ml-2">Something went wrong...</span>}
             </p>
             <form
                 className="flex-row justify-center justify-space-between-md align-stretch"
--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -4,6 +4,9 @@ import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHT } from '../utils/queries';
 
 import ReactionList from '../components/ReactionList';
+import ReactionForm from '../components/ReactionForm';
+
+import Auth from '../utils/auth';
 
 const SingleThought = props => {
   const { id: thoughtId } = useParams();
@@ -33,6 +36,7 @@ const SingleThought = props => {
       </div>
 
       {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
     </div>
   );
 };
